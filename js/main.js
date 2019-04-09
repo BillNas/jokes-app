a11c376587edcbb0f272c75cd5ca1f3df66c58d8
BillNas <37874504+BillNas@users.noreply.github.com>
2019-04-10 00:47:29 +0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,26 @@
+function getJoke(){
+    var category = document.querySelector('#category').value;
+
+    axios.get(`https://sv443.net/jokeapi/category/${category}`)
+        .then(response => {
+            showJokes(response.data);
+            console.log(response.data)
+        })
+        .catch(err => console.log(err));
+}
+function showJokes(joke){
+    const results = document.querySelector('#results');
+    results.innerHTML = '';
+    if(joke.type==='single'){
+        results.innerHTML = `
+          ${joke.joke} `
+    }
+    else{
+        results.innerHTML = `
+          ${joke.setup}
+          <br>
+         ${joke.delivery}
+          `
+
+    }
+}
